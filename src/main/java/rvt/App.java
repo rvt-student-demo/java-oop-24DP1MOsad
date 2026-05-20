5dbda41a0583e1a57542cf5230e541cb38c36b5a
24DP1MOsad <a240503mo@rvt.lv>
2026-05-20 14:40:08 +0300

    db

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,10 +1,25 @@
 package rvt;
 
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+
 public class App {
     public static void main(String[] args) {
-        ToDoList list = new ToDoList();
-        UserInterface i = new UserInterface(list);
-        i.start();
+        final String url = "jdbc:sqlite:todo.db";
+        try (
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+        ) {
+            String sql = "CREATE TABLE IF NOT EXISTS todo"
+                + "(id INTEGER PRIMARY KEY, task TEXT NOT NULL) STRICT";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 	}
 }
 
